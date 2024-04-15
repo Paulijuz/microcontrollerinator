c7b7fc42dfd5f0ec762c615821dd685f3857ceaa
Paulius Juzenas <59730296+Paulijuz@users.noreply.github.com>
2024-04-15 15:05:07 +0200

    update stuff

--- 1_gpio/main.c
+++ 1_gpio/main.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 
-#define GPIO ((NRF_GPIO_REGS*)0x50000000)
+#define GPIO ((NRF_GPIO_REGS*)__GPIO_BASE_ADDRESS__)
 
 typedef struct {
 	volatile uint32_t RESERVED0[321];
@@ -13,98 +13,34 @@ typedef struct {
 	volatile uint32_t DIRCLR;
 	volatile uint32_t LATCH;
 	volatile uint32_t DETECTMODE;
-	volatile uint32_t RESERVED1[118];
+	volatile uint32_t RESERVED1[__RESERVED1_SIZE__];
 	volatile uint32_t PIN_CNF[32];
 } NRF_GPIO_REGS;
 
 void button_init(){ 
-	for (int i = 13; i <= 16; i++) {
-		GPIO->PIN_CNF[i] = (3 << 2);
-	}
+	GPIO->PIN_CNF[__BUTTON_1_PIN__] = (3 << 2);
+	// Fill inn the configuration for the remaining buttons
 }
-
-int i = 0;
-
-typedef enum mode {
-	BLINK,
-	SNAKE,
-
-	BLINK_ENUM_LEN,
-} mode_t;
-
-mode_t blink_mode = SNAKE;
 
 int main(){
 	// Configure LED Matrix
 	for(int i = 17; i <= 20; i++){
-		GPIO->DIRSET |= (1 << i);
-		// GPIO->OUTCLR |= (1 << i);
-		GPIO->OUT |= (1 << i);
+		GPIO->DIRSET = (1 << i);
+		GPIO->OUTCLR = (1 << i);
 	}
 
 	// Configure buttons -> see button_init()
-	button_init();
-	
+
 	int sleep = 0;
-	int leds[4] = {17, 18, 20, 19};
 	while(1){
-		int led;
-		switch (blink_mode) {
-			case BLINK:
-				if (i++ % 2 == 0) {
-					for(int i = 17; i <= 20; i++){
-						GPIO->OUT &= ~(1 << i);
-					}
-				} else {
-					for(int i = 17; i <= 20; i++){
-						GPIO->OUT |= (1 << i);
-					}
-				}
-				break;
-
-			case SNAKE:
-				led = leds[i++ % 4];
-				for( int i = 17; i <= 20; i++){
-					if (i == led) GPIO->OUT &= ~(1 << i);
-					else GPIO->OUT |= (1 << i);
-				}
-				break;
-			default:
-				break;
-		}
-
-		// if (i % 100 == 0) blink_mode = ((blink_mode ++) % BLINK_ENUM_LEN);
-		if (i % 16 == 0) {
-			switch (blink_mode) {
-				case BLINK:
-					blink_mode = SNAKE;
-					break;
-				case SNAKE:
-					blink_mode = BLINK;
-					break;
-				default:
-					blink_mode = SNAKE;
-					break;
-			}
-		}
 
 		/* Check if button 1 is pressed;
 		 * turn on LED matrix if it is. */
-		if (!(GPIO->IN & (1 << 13))) {
-			for(int i = 17; i <= 20; i++){
-				GPIO->OUT &= ~(1 << i);
-			}
-		}
 
 		/* Check if button 2 is pressed;
 		 * turn off LED matrix if it is. */
-		if (!(GPIO->IN & (1 << 14))) {
-			for(int i = 17; i <= 20; i++){
-				GPIO->OUT |= (1 << i);
-			}
-		}
 
-		sleep = 500000;
+		sleep = 10000;
 		while(--sleep); // Delay
 	}
 	return 0;
--- 3_gpiote/gpiote.h
+++ 3_gpiote/gpiote.h
@@ -6,22 +6,19 @@
 
 typedef struct {
 	// Tasks
-	volatile uint32_t OUT[8];
+	volatile uint32_t OUT[4];
 	// Events
-	volatile uint32_t RESERVEDMINUS2[4];
-	volatile uint32_t SET[8];
-	volatile uint32_t RESERVEDMINUS1[4];
-	volatile uint32_t CLR[8];
-	volatile uint32_t RESERVED0[32];
-	volatile uint32_t IN[8];
-	volatile uint32_t RESERVED1[23];
+	volatile uint32_t RESERVED0[60];
+	volatile uint32_t IN[4];
+	volatile uint32_t RESERVED1[27];
 	volatile uint32_t PORT;
 	// Registers
-	volatile uint32_t RESERVED2[97];
+	volatile uint32_t RESERVED2[96];
+	volatile uint32_t INTEN;
 	volatile uint32_t INTENSET;
 	volatile uint32_t INTENCLR;
 	volatile uint32_t RESERVED3[129];
-	volatile uint32_t CONFIG[8];
+	volatile uint32_t CONFIG[4];
 } NRF_GPIOTE_REG;
 
 #endif
